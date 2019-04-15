ef40724e2c58320e885f556b7d2305a3a801ad06
Oleg-88 <oleg.russak88@gmail.com>
2019-04-15 13:40:21 +0200

    Добавлены списки и объявления.

--- index.php
+++ index.php
@@ -56,40 +56,40 @@ $user_name = 'Олег Русак';
 <?php
     $items = [
         [
-                'name' => '2014 Rossignol District Snowboard',
-                'category' => 'Доски и лыжи',
-                'price' => '10999',
-                'pic-url' => 'img/lot-1.jpg'
+            'name' => '2014 Rossignol District Snowboard',
+            'category' => 'Доски и лыжи',
+            'price' => '10999',
+            'image' => 'img/lot-1.jpg'
         ],
         [
             'name' => 'DC Ply Mens 2016/2017 Snowboard',
             'category' => 'Доски и лыжи',
             'price' => '159999',
-            'pic-url' => 'img/lot-2.jpg'
+            'image' => 'img/lot-2.jpg'
         ],
         [
             'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
             'category' => 'Крепления',
             'price' => '8000',
-            'pic-url' => 'img/lot-3.jpg'
+            'image' => 'img/lot-3.jpg'
         ],
         [
             'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
             'category' => 'Ботинки',
             'price' => '10999',
-            'pic-url' => 'img/lot-4.jpg'
+            'image' => 'img/lot-4.jpg'
         ],
         [
             'name' => 'Куртка для сноуборда DC Mutiny Charocal',
             'category' => 'Одежда',
             'price' => '7500',
-            'pic-url' => 'img/lot-5.jpg'
+            'image' => 'img/lot-5.jpg'
         ],
         [
             'name' => 'Маска Oakley Canopy',
             'category' => 'Разное',
             'price' => '5400',
-            'pic-url' => 'img/lot-6.jpg'
+            'image' => 'img/lot-6.jpg'
         ]
     ];
 ?>
@@ -115,18 +115,18 @@ $user_name = 'Олег Русак';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+            <?php foreach ($items as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$val['image']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$val['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$val['price'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -134,6 +134,7 @@ $user_name = 'Олег Русак';
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
