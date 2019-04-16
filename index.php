d74eb3b2afbf2a850c91b9c84a71894ae0c9fba8
Oleg-88 <oleg.russak88@gmail.com>
2019-04-16 17:47:49 +0200

    Функция.

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ $user_name = 'Олег Русак';
     function prices($item)
         {   $price0 = ceil($item);
             if ($price0 < 1000) {
-                $price_0 = number_format($price0, 0, '', ' ');
+                $price_0 = number_format($price0, 0, "", " ");
                 } else {
                 $price_0 = $price0;
                 }
