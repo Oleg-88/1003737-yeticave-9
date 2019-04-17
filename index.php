4490f196e6a6460983fcffec77e67082797e07ba
Oleg-88 <oleg.russak88@gmail.com>
2019-04-17 10:33:46 +0200

    Функция.

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $user_name = 'Олег Русак';
     ];
     function prices($item) {
         $price_form = ceil($item);
-        $price_form = number_format($price0, 0, "", " ") . ' ₽';
+        $price_form = number_format($price0, 0, "", " ") . " ₽";
 ?>
 
 <main class="container">
@@ -122,7 +122,7 @@ $user_name = 'Олег Русак';
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <?php print(prices($item['price']));?>
+                            <?php print prices($item['price']);?>
                         </div>
                         <div class="lot__timer timer">
                             12:23
