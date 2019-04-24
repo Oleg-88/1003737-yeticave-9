95a180ad348b8a6d7393d7055e20e0972bc05d38
Oleg-88 <oleg.russak88@gmail.com>
2019-04-24 13:43:01 +0200

    Схема баззы данных

--- schema.sql
+++ schema.sql
@@ -6,21 +6,21 @@ USE yeticave;
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(64) NOT NULL -- название
+    name VARCHAR(128) NOT NULL -- название
 );
 
 CREATE TABLE lots (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- дата размещения
-    name CHAR(64) NOT NULL, -- название
+    name VARCHAR(128) NOT NULL, -- название
     description TEXT NOT NULL, -- описание
-    image CHAR(128) NOT NULL, -- изображение
+    image VARCHAR(128) NOT NULL, -- изображение
     price DECIMAL NOT NULL, -- стартоваая цена
     end_date TIMESTAMP NOT NULL, -- дата завершения
     bid_step DECIMAL NOT NULL DEFAULT 100, -- шаг ставки
     user_id INT NOT NULL, -- автор, связь с users
-    winner_id INT NOT NULL, -- категория, связь с users
-    category_id INT NOT NULL -- победитель, связь с categories
+    winner_id INT, -- победитель, связь с users
+    category_id INT NOT NULL -- категория, связь с categories
 );
 
 CREATE TABLE bids (
@@ -34,16 +34,14 @@ CREATE TABLE bids (
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- дата регистрации
-    email CHAR(64) NOT NULL UNIQUE, -- адрес электронной почты
-    name CHAR(64) NOT NULL, -- имя пользователя
-    password CHAR(64) NOT NULL, -- пароль
-    avatar CHAR(128), -- аватар пользователя
+    email VARCHAR(128) NOT NULL UNIQUE, -- адрес электронной почты
+    name VARCHAR(128) NOT NULL, -- имя пользователя
+    password VARCHAR(128) NOT NULL, -- пароль
+    avatar VARCHAR(128), -- аватар пользователя
     contact TEXT NOT NULL, -- контактные данные
-    lot_id INT NOT NULL, -- лоты пользователя, связь с lots
-    bid_id INT NOT NULL -- лоты пользователя, связь с bids
+    lot_id INT NOT NULL, -- лоты пользователя
+    bid_id INT NOT NULL -- лоты пользователя
 );
-
-CREATE UNIQUE INDEX u_email ON users(email); -- уникальный индекс для почтовых адресов
 
 CREATE INDEX l_category ON lots(category_id); -- индекс для поиска по категория
 CREATE INDEX l_name ON lots(name) -- индекс для поиска по названиям лотов
